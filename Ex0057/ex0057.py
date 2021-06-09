5c8a3012e07962b3bbc8b366663f03db9a876007
Rodrigo-Antonio-Silva <rodrigo2013.rs33@gmail.com>
2021-06-08 23:09:51 -0300

    Update ex0057.py

--- Ex0057/ex0057.py
+++ Ex0057/ex0057.py
@@ -1,4 +1,12 @@
-sex = str(input('Qual o sexo [M/F]:').upper().strip())
-while sex not in 'MmFf':
-    sex = str(input('Dados inválidos. Por favor, digite novamente:' ).upper().strip())
-print(sex)
+#utf-8
+
+while True:
+    sex = str(input('Informe o Sexo [M/F]: ')).upper()
+    if sex != 'M' and sex != 'F':
+        print('Dados inválidos. ')
+    else:
+        if sex == 'M':
+            print('Masculino')
+        else:
+            print('Feminino')
+        break
