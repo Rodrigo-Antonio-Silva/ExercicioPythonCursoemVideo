6af4f62ca5721b20d99dec00932866513a3093d3
Rodrigo-Antonio-Silva <rodrigo2013.rs33@gmail.com>
2021-06-10 16:53:05 -0300

    Update ex0063.py

--- Ex0063/ex0063.py
+++ Ex0063/ex0063.py
@@ -1,13 +1,22 @@
 #utf-8
-#Sequencia de Fibonati
-n = int(input('Insira um nº: '))
-f = 0
-i = 0
-while i < n:
-    if f > 3:
-        print(f-2)
-    else:
-        print(f)
-    f += f
-    f+= 1
-    i+= 1
+#Sequencia de Fibonacci
+
+term = int(input('Quantos termos da sequência Fibonacci deseja ver: '))
+zero = 0
+one = 1
+j= 0
+fi = []
+fi.append(zero)
+fi.append(one)
+while j < (term - 2):
+    sum = fi[zero] + fi[one]
+    fi.append(sum)
+    zero += 1
+    one += 1
+    j += 1
+z = 0
+for i in fi:
+    print(i, end='')
+    print(end=' → ' if z < len(fi) else print(end=' FIM'))
+    z += 1
+print('FIM')
